753328215b23785f443dcb86e5ea55702c14ae78
Mariawx <grzeszcyszen.maria@escola.pr.gov.br>
2025-09-15 11:46:24 -0300

    .

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ function processaTexto(texto) {
 
     const frequencias = contaFrequencias(palavras);
 
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
+
     function mostraPalavrasChave() { 
   let palavras = texto.split(/\P{L}+/u);
 
